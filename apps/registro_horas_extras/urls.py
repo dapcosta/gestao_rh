b36fea24dae26c3748438d41359ae30e352f759b
dapcosta <dapcosta@hotmail.com>
2021-04-08 23:44:52 -0300

    Adicionando .gitignore e requirements

--- apps/registro_horas_extras/urls.py
+++ apps/registro_horas_extras/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from .views import (
-    HoraExtraList
+    HoraExtraList,
+    HoraExtraEdit,
+    HoraExtraDelete,
+    HoraExtraNovo,
 )
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
-
+    path('novo/', HoraExtraNovo.as_view(), name='create_registro_hora_extra'),
+    path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
+    path('deletar/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
 ]
