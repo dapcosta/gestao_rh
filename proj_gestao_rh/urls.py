6fa5c6cefc97784b19bb7c4b6cc3a16d32ff424c
dapcosta <dapcosta@hotmail.com>
2021-04-27 00:22:00 -0300

    Expondo api Django Rest Framework

--- proj_gestao_rh/urls.py
+++ proj_gestao_rh/urls.py
@@ -1,7 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from rest_framework import routers
 from django.urls import path, include
+
+from apps.core import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include('apps.core.urls')),
@@ -12,4 +19,8 @@ urlpatterns = [
     path('empresa/', include('apps.empresas.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
